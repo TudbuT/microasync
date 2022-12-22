58cec36399801bc8697d6ad8351fbb2b7e833a57
TudbuT <tudbut@tudbut.de>
2022-12-22 18:04:26 +0100

    remove redundant dependency

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,18 @@
-use std::{future::Future, task::Context, thread};
+use std::{future::Future, task::{Context, Waker, RawWaker, RawWakerVTable, Poll}, thread, ptr::null};
 
-use futures_task::{Poll, noop_waker_ref};
+fn empty(_: *const ()) {}
+fn empty_clone(_: *const ()) -> RawWaker {
+    empty_raw_waker()
+}
+
+fn empty_raw_waker() -> RawWaker {
+    RawWaker::new(null(), &RawWakerVTable::new(empty_clone, empty, empty, empty))
+}
+
+fn empty_waker() -> Waker {
+    // SAFETY: Can't fail because no dynamic data is used and this function was tested.
+    unsafe { Waker::from_raw(empty_raw_waker()) }
+}
 
 pub fn sync<T>(future: impl Future<Output = T> + 'static) -> T {
     // Initialize things
@@ -10,7 +22,8 @@ pub fn sync<T>(future: impl Future<Output = T> + 'static) -> T {
     loop {
         // Usage of thread::yield_now combined with this being a single-task runtime makes the
         // waker redundant.
-        let context = &mut Context::from_waker(noop_waker_ref());
+        let waker = empty_waker();
+        let context = &mut Context::from_waker(&waker);
         // If the future is done, stop and return.
         if let Poll::Ready(content) = future.as_mut().poll(context) {
             return content;
