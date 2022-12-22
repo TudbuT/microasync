2db63e574d268e072c83c52d979b992e79904b87
TudbuT <tudbut@tudbut.de>
2022-12-22 17:57:04 +0100

    improve performance and remove redundant code

--- src/lib.rs
+++ src/lib.rs
@@ -1,39 +1,16 @@
-use std::{cell::UnsafeCell, future::Future, pin::Pin, sync::Arc, task::Context, thread};
+use std::{future::Future, task::Context, thread};
 
-use futures_task::{waker_ref, ArcWake, Poll};
-
-type BoxFuture<T> = Pin<Box<dyn Future<Output = T> + 'static>>;
-
-struct MicroTask<T> {
-    future: UnsafeCell<BoxFuture<T>>,
-    sync_thread: thread::Thread,
-}
-
-impl<T> ArcWake for MicroTask<T> {
-    fn wake_by_ref(arc_self: &Arc<Self>) {
-        // SAFETY: Can't fail because the runner only stops once the future is done.
-        arc_self.sync_thread.unpark();
-    }
-}
-
-// SAFETY: These will never actually be used! This runner is single-threaded.
-unsafe impl<T> Send for MicroTask<T> {}
-unsafe impl<T> Sync for MicroTask<T> {}
+use futures_task::{Poll, noop_waker_ref};
 
 pub fn sync<T>(future: impl Future<Output = T> + 'static) -> T {
     // Initialize things
-    let task = Arc::new(MicroTask {
-        future: UnsafeCell::new(Box::pin(future)),
-        sync_thread: thread::current(),
-    });
+    let mut future = Box::pin(future);
 
     // Now actually run the future.
     loop {
-        // SAFETY: Access to the future field is single-threaded (unlike the sync_thread field),
-        // therefore we can access it mutably.
-        let future = unsafe { task.future.get().as_mut().unwrap() };
-        let waker = waker_ref(&task);
-        let context = &mut Context::from_waker(&waker);
+        // Usage of thread::yield_now combined with this being a single-task runtime makes the
+        // waker redundant.
+        let context = &mut Context::from_waker(noop_waker_ref());
         // If the future is done, stop and return.
         if let Poll::Ready(content) = future.as_mut().poll(context) {
             return content;
